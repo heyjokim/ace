b36bf5e4f6e941ccfeac7f69330e1994252e7de9
heyjokim <90989987+heyjokim@users.noreply.github.com>
2022-07-10 18:00:44 -0400

    added support for context menu

--- src/preload.js
+++ src/preload.js
@@ -11,4 +11,7 @@ contextBridge.exposeInMainWorld('portgasAPI', {
   lookupIOC: (object) => ipcRenderer.sendSync('search:lookupIOC', object),
   lookupResults: () => ipcRenderer.sendSync('search:iocResults'),
   downloadResults: () => ipcRenderer.invoke('dialog:downloadResults'),
+  showContextMenu: (object) => ipcRenderer.send('show-context-menu', object),
+  returnContextMenuData: (type, object) =>
+    ipcRenderer.once('context-menu-command', type, object),
 });
